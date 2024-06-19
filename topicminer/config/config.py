6f9271ca35fbc32128b531e0e354147dfd26f51a
AaronNHorvitz <AaronNHorvitz@gmail.com>
2024-06-19 15:24:25 -0500

    Initial commit of the config.py file.

---
+++ topicminer/config/config.py
@@ -0,0 +1,22 @@
+import os
+
+# Absolute path to the directory containing the config.py file
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# Path definitions for various resources
+DATA_DIR = os.path.join(BASE_DIR, 'data')
+RAW_DATA_DIR = os.path.join(DATA_DIR, 'raw_data')
+PROCESSED_DATA_DIR = os.path.join(DATA_DIR, 'processed_data')
+UNWANTED_TEXTS_FILE = os.path.join(DATA_DIR, 'unwanted_texts', 'unwanted_texts.json')
+
+# Example configuration for logging
+LOG_DIR = os.path.join(BASE_DIR, 'logs')
+LOG_FILE = os.path.join(LOG_DIR, 'topicminer.log')
+
+# Ensure that essential directories exist
+os.makedirs(RAW_DATA_DIR, exist_ok=True)
+os.makedirs(PROCESSED_DATA_DIR, exist_ok=True)
+os.makedirs(LOG_DIR, exist_ok=True)
+
+# If needed, additional configurations such as database settings,
+# API keys, or environment-specific configurations can be added here
